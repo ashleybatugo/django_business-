bcc9eb8fd0bd01d429dabb7c4b0d48e1e9c59fc7
Andrea Batugo <abatugo@Andreas-MBP.fios-router.home>
2020-06-18 12:38:04 -0400

    initial commit

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,14 @@ import Hoc from "./hoc/hoc";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import HomepageLayout from "./containers/Home";
+import Demo from "./containers/Form";
 
 const BaseRouter = () => (
   <Hoc>
     <Route path="/login" component={Login} />
     <Route path="/signup" component={Signup} />
     <Route exact path="/" component={HomepageLayout} />
+    <Route exact path='/form2' component={Demo} />
   </Hoc>
 );
 
